35c8817290c917a6017a3bc7071856beab6e584e
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2020-04-06 18:41:18 +0530

    New screen to display claim details

---
+++ lib/screens/claim_request_details.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = Firestore.instance;
+
+class ClaimRequestDetails extends StatefulWidget {
+  static const id = 'claim_request_details';
+  final FirebaseUser currentUser;
+
+  ClaimRequestDetails(this.currentUser);
+  @override
+  _ClaimRequestDetailsState createState() => _ClaimRequestDetailsState();
+}
+
+class _ClaimRequestDetailsState extends State<ClaimRequestDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
