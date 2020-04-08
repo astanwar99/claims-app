d7746667b3542598d9864350279a62ef33698d16
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2020-04-08 17:40:47 +0530

    claim_request_details: Core logic
    Add functionality to claim claim_request_details.dart

--- lib/screens/claim_request_details.dart
+++ lib/screens/claim_request_details.dart
@@ -1,21 +1,60 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-final _firestore = Firestore.instance;
+import 'package:flutter/material.dart';
 
 class ClaimRequestDetails extends StatefulWidget {
   static const id = 'claim_request_details';
-  final String client;
+  final DocumentSnapshot requestDetails;
 
-  ClaimRequestDetails(this.client);
+  ClaimRequestDetails(this.requestDetails);
   @override
   _ClaimRequestDetailsState createState() => _ClaimRequestDetailsState();
 }
 
 class _ClaimRequestDetailsState extends State<ClaimRequestDetails> {
+  String _title;
+  String _description;
+  double _amount;
+  DateTime _date;
+  String _billUrl;
+  String _user;
+
+  @override
+  void initState() {
+    super.initState();
+    getDetails();
+  }
+
+  void getDetails() {
+    _title = widget.requestDetails.data['title'];
+    _description = widget.requestDetails.data['description'];
+    _amount = widget.requestDetails.data['amount'];
+    _date = widget.requestDetails.data['date'].toDate();
+    _billUrl = widget.requestDetails.data['billUrl'];
+    _user = widget.requestDetails.data['user'];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Claims Form'),
+      ),
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Column(
+          children: <Widget>[
+            Text('User: $_user'),
+            Text('Title: $_title'),
+            Text('Description: $_description'),
+            Text('Date: ${_date.toString()}'),
+            Text('Amount: ${_amount.toString()}'),
+            _billUrl != null
+                ? Image.network(_billUrl)
+                : Text('Bill not available'),
+          ],
+        ),
+      ),
+    );
   }
 }
